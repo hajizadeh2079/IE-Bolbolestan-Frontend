ace3018dfaee7eeaeda55ff675945ecd5e428878
hajizadeh2079 <hajizadeh2079@gmail.com>
2021-04-25 23:14:02 +0430

    Add error404 page

--- src/common/Error404.js
+++ src/common/Error404.js
@@ -1,13 +1,19 @@
-import { React, Component } from "react";
+import React from "react";
+import "./Error404.css";
+import logo from "../common/photos/logo.png";
 
-class Error404 extends Component {
-  render() {
-    return <h1>Error404</h1>;
-  }
-
-  getId = () => {
-    return JSON.parse(localStorage.getItem("id"));
-  };
-}
+const Error404 = () => {
+  return (
+    <div className="my-container error-container error-container-cover">
+      <div className="error-card borders">
+        <img src={logo} alt="" className="error-form-logo" />
+        <h2>صفحه‌ی مورد نظر وجود ندارد!</h2>
+        <h3>
+          <a href="/">بازگشت به خانه</a>
+        </h3>
+      </div>
+    </div>
+  );
+};
 
 export default Error404;
