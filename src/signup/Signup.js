aa9d5a88ebc05ca0bbb85d0fd533567b8b423ed7
armin__af <armin.afshariian@gmail.com>
2021-04-25 18:01:59 +0430

    Complete building signup page

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -1,13 +1,78 @@
 import { React, Component } from "react";
+import "./Signup.css";
+import logo from "../common/photos/logo.png";
+import { Redirect } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 
-class Signup extends Component {
+class signup extends Component {
   render() {
-    return <h1>Signup</h1>;
+    const id = this.getId();
+    if (id) return <Redirect to={{ pathname: "/" }} />;
+    return (
+      <div className="my-container signup-container container-cover">
+        <div className="signup-card borders">
+          <img src={logo} alt="" className="signup-form-logo" />
+          <form className="signup-form" onSubmit={this.handleSubmit}>
+            <input
+              className="borders signup-form-input"
+              onChange={this.handleInputChange}
+              name="name"
+              type="text"
+              id="inputName"
+              placeholder="نام"
+              required
+              autoFocus
+            />
+            <input
+              className="borders signup-form-input"
+              onChange={this.handleInputChange}
+              name="secondName"
+              type="text"
+              id="inputSecondName"
+              placeholder="نام خانوادگی"
+              required
+            />
+            <input
+              className="borders signup-form-input"
+              onChange={this.handleInputChange}
+              name="email"
+              type="text"
+              id="inputEmail"
+              placeholder="آدرس ایمیل"
+              required
+            />
+            <input
+              className="borders signup-form-input"
+              onChange={this.handleInputChange}
+              name="password"
+              type="password"
+              id="inputPassword"
+              placeholder="رمز عبور"
+              required
+            />
+            <button className="borders signup-button" type="submit">
+              ثبت نام
+            </button>
+            <span className="has-account">
+              آیا قبلا ثبت نام کرده اید؟ <a href="/login">ورود</a>
+            </span>
+          </form>
+        </div>
+      </div>
+    );
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleInputChange = (event) => {
+    // Todo
+  };
 
   getId = () => {
     return JSON.parse(localStorage.getItem("id"));
   };
 }
 
-export default Signup;
+export default signup;
